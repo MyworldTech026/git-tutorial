026ca2830e7001975ab70cc937b58d18b76aa649
myworldTech026 <muhammedzulikaneni2004@gmail.com>
2026-09-21 18:00:19 +0100

    version 4 updated

--- calc.js
+++ calc.js
@@ -2,7 +2,7 @@ document.querySelectorAll('.js-button').forEach((button)=>{
 button.addEventListener('click',(e)=>{
   let btn=button.dataset.number
   
-  
+  console.log(`version 5`)
  
   document.querySelector('.js-display').value=btn
 })
--- change.js
+++ change.js
@@ -1,1 +1,3 @@
 console.log('change')
+console.log(`version 5`)
+console.log(`am learning git and github today`)

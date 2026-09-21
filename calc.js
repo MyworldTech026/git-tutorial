b362185ae360896e998685403318879b60b45f1d
myworldTech026 <muhammedzulikaneni2004@gmail.com>
2026-09-21 12:21:56 +0100

    version 2

--- calc.js
+++ calc.js
@@ -1,8 +1,8 @@
 document.querySelectorAll('.js-button').forEach((button)=>{
-button.addEventListener('click',(e)=>{
+button.addEventListener('click',(event)=>{
   let btn=button.dataset.number
   
-  
+  console.log(`hello`)
  
   document.querySelector('.js-display').value=btn
 })
